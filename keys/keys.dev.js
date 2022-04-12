b11a7e23782fa6c573b009900779ff65b67e3fd6
Ilia Kozionov <68503974+Pardjai@users.noreply.github.com>
2022-04-12 19:28:06 +0300

    Add files via upload

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -4,8 +4,8 @@ module.exports = {
       EMAIL_FROM: 'courses-collect@email.com',
    EMAIL_USER: 'ik.ilyakozionov@gmail.com',
    EMAIL_CLIENT_ID:
-      '189696599129-fbtnihmlvckbglvq3e87ktuq9b17ddt6.apps.googleusercontent.com',
-   EMAIL_CLIENT_SECRET: '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',
+      '399011256031-tnm16iu1f3tdovj5ehlnl4ih8ucdi0rj.apps.googleusercontent.com',
+   EMAIL_CLIENT_SECRET: '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',
    EMAIL_REFRESH_TOKEN:
-      '1//04KP9WXJooK_ZCgYIARAAGAQSNwF-L9IrrLiseGW1yU3Ol8Vz6U0b5rlnDWoevOPh24uMVXQf3qRSGovszPNUq-BIKaffqBOVoDM',
+      '1//04N2GQwrNbPXzCgYIARAAGAQSNwF-L9IrmMLQSqLl9q4vt9zaLART30r_DMzTBIw_OaE7KnlC4iNWI7XmPspTy-HAyVqGM9B3PDM',
 };
